19ab836ca4710f86737a0d228ec9e0ef6110b64c
Siew Kam Onn <kosiew@gmail.com>
2024-06-20 17:55:08 +0800

    blog/list url

--- Chapter01/mysite/blog/urls.py
+++ Chapter01/mysite/blog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path("list", views.post_list, name="post_list"),
+    path("<int:id>/", views.post_detail, name="post_detail"),
 ]
